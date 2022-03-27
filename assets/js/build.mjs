febf9cf402b4d3f4d7d32f57537f4cc5470bed7e
bwireman <bwireman1@gmail.com>
2022-03-27 12:02:27 -0500

    builds css with esbuild

--- assets/js/build.mjs
+++ assets/js/build.mjs
@@ -2,9 +2,9 @@ import esbuild from "esbuild"
 import esgleam from "esgleam"
 
 esbuild.build({
-    entryPoints: ['./src/main.gleam'],
+    entryPoints: ['./src/main.gleam', '../styles.css'],
     bundle: true,
-    outfile: 'out.js',
+    outdir: 'out',
     minify: true,
     plugins: [esgleam.esgleam({ main_function: "main", project_root: "." })],
 }).catch(() => process.exit(1))
