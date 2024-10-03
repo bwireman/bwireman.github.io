01b20b4f200fac20dbe232f407bfd892fbca405d
bwireman <bwireman1@gmail.com>
2024-10-03 18:38:28 -0500

    chatbox!

--- src/lib/Snark.svelte
+++ src/lib/Snark.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
   import {Viewable} from "@svelte-plugins/viewable"
-  const randAssign = (choices: string[], quote = true): string => {
-    const c = choices[Math.floor(Math.random() * choices.length)]
-
-    if (quote) {
-      return `"${c}"`
-    }
-
-    return c
+  const randAssign = <T,>(choices: T[]): T => {
+    return choices[Math.floor(Math.random() * choices.length)]
   }
 
-  const emoji = randAssign(["🐙", "🐹", "🐲"], false)
-  const firstMessage = randAssign(["Hey!", "Shoo!"])
+  const [emoji, name] = randAssign([
+    ["🐙", "Scary Octopus"],
+    ["🐹", "Angry Hamster"],
+    ["🐲", "Guardian Dragon"],
+    ["👾", "Invader"]
+  ])
+  const firstMessage = randAssign(["Hey!", "Shoo!", "OI!"])
   const midMessage = randAssign(["What are you doing here!?", "Why are you here?"])
   const endMessage = randAssign(["Go away!", "The stuffs up there!", "Leave!"])
 
@@ -35,32 +34,51 @@
 </script>
 
 <Viewable {rules} element={emojiElement}>
-  <div bind:this={emojiElement} style="text-align: center; padding: 1rem;">
-    {#if lvl > 0}
-      <p style="font-size: 3.5rem" class="snark">{emoji}</p>
-    {/if}
-
-    {#if lvl > 1}
-      <p class="snark">{firstMessage}</p>
-    {/if}
-    {#if lvl > 2}
-      <p class="snark">{midMessage}</p>
-    {/if}
-    {#if lvl > 3}
-      <p class="snark">{endMessage}</p>
-    {/if}
+  <div class="columns is-centered">
+    <div bind:this={emojiElement} class="column is-one-fifth" style="text-align: center; padding: 1rem;">
+      {#if lvl > 0}
+        <p style="font-size: 3.5rem" class="snark">{emoji}</p>
+      {/if}
+      {#if lvl > 1}
+        <div class="chatbox">
+          <p class="name">{name}</p>
+          <p class="snark">{firstMessage}</p>
+          {#if lvl > 2}
+            <p class="snark">{midMessage}</p>
+          {/if}
+          {#if lvl > 3}
+            <p class="snark">{endMessage}</p>
+          {/if}
+        </div>
+      {/if}
+    </div>
   </div>
   <div class="snark" bind:this={endElement}></div>
 </Viewable>
-<!-- 
+
 <style lang="scss">
   .chatbox {
-    border-color: pink;
-    border-width: 1rem;
+    font-family: "JetBrains Mono", monospace;
+    border-style: solid;
+    border-color: #61656e;
+    border-width: 0.5rem;
     border-radius: 1rem;
     text-align: left;
-    padding: 1rem;
+    padding: 0.5rem;
     color: whitesmoke;
     background-color: #2e333d;
   }
-</style> -->
+
+  .chatbox > .name {
+    float: right;
+    text-align: center;
+    position: relative;
+    top: -1.5rem;
+    color: whitesmoke;
+    background-color: #61656e;
+    max-width: fit-content;
+    padding: 0rem 0.25rem 0rem 0.25rem;
+    border-radius: 0.25rem;
+    font-family: "JetBrains Mono", monospace;
+  }
+</style>
